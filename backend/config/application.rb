d38e7f302f136ec35938fbea3040d326446adc24
Filipe F. Kalicki <filipe.kalicki@gmail.com>
2018-03-03 19:25:35 -0300

    Update Routes/DB for GraphQL

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -29,5 +29,9 @@ module App
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.autoload_paths << Rails.root.join('app/graphql')
+    config.autoload_paths << Rails.root.join('app/graphql/types')
+    config.autoload_paths << Rails.root.join('app/graphql/mutations')
   end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "application#index"
+  post "/graphql", to: "graphql#create"
+  
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
+  end
 end
